a313b6e0a9b2b7e71cbc7a6a246f10dec3dea2b3
Thiago Batista <tsb.thiagobatista@gmail.com>
2022-12-03 08:35:19 -0300

    style: desafio-01: correção responsividade

--- desafio-01/src/components/Header/style.js
+++ desafio-01/src/components/Header/style.js
@@ -1,15 +1,19 @@
 import styled from "styled-components";
 
 export const HeaderStyle = styled.header`
+  width: 100%;
   background: linear-gradient(to right, #574ae8, #3ea1db);
 `;
 
 export const Container = styled.div`
   max-width: 144rem;
   margin: 0 auto;
-  padding: 4rem 18%;
+  padding: 4rem 26rem;
 
-  @media screen and (max-width: 1200px){
+  @media screen and (max-width: 1440px) {
+    padding: 4rem 18%;
+  }
+  @media screen and (max-width: 1200px) {
     padding: 4rem 12%;
   }
   @media screen and (max-width: 600px) {
@@ -25,7 +29,7 @@ export const LogoBox = styled.div`
   @media screen and (max-width: 600px) {
     margin-bottom: 4rem;
     .logo {
-        width: 10rem;
+      width: 10rem;
     }
   }
 `;
--- desafio-01/src/components/Main/style.js
+++ desafio-01/src/components/Main/style.js
@@ -3,10 +3,13 @@ import styled from "styled-components";
 export const Container = styled.main`
   max-width: 144rem;
   margin: 10rem auto;
-  padding: 0 18%;
+  padding: 0 26rem;
 
+  @media screen and (max-width: 1440px){
+    padding: 0 18%;
+  }
   @media screen and (max-width: 1200px) {
-    padding: 0rem 12%;
+    padding: 0 12%;
   }
   @media screen and (max-width: 600px) {
     margin: 5rem auto;
